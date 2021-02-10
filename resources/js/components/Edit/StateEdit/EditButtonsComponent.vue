be8af5a403bbcc32866359dabac1cdb3bc7715de
Futo Nakajima <maple0922wh-142857@i.softbank.jp>
2021-02-10 23:48:22 +0900

    Add: submit関数を実装したところまで

--- resources/js/components/Edit/StateEdit/EditButtonsComponent.vue
+++ resources/js/components/Edit/StateEdit/EditButtonsComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card-actions class="flex-row justify-end">
-    <v-btn color="primary" @click="update">Update</v-btn>
+    <v-btn color="primary" @click="submit">Update</v-btn>
     <v-btn :to="{ name: 'home' }">Cancel</v-btn>
   </v-card-actions>
 </template>
@@ -9,15 +9,26 @@
 export default {
   name: "StateButtonsComponent",
   props: ["id"],
+
+  data: () => ({
+    state: {},
+  }),
+
   methods: {
-    update: () => {
-      if (confirm("更新してよろしいですか？")) {
-        alert("Updated!!");
-      }
+    getState() {
+      axios.get("/api/states/" + this.id).then((res) => {
+        this.state = res.data;
+      });
+    },
+    submit() {
+      axios.put("/api/states/" + this.id, this.state).then((res) => {
+        this.$router.push({ name: "home" });
+      });
     },
   },
   mounted() {
     console.log(typeof this.id);
+    this.getState();
   },
 };
 </script>
